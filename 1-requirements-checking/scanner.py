fdefabccd2bc52756142b8a335afbc2eee3a288b
Basil Rommens <basil.rommens@student.uantwerpen.be>
2021-10-14 17:07:39 +0200

    added new scanner files

--- 1-requirements-checking/scanner.py
+++ 1-requirements-checking/scanner.py
@@ -4,15 +4,47 @@
 # Based on code for COMP 304B Assignment 3
 # Updated to Python 3 in 2021
 #
-
+import re
 # trace FSA dynamics (True | False)
 __trace__ = False
-# __trace__ = True
+
+
+class LineStream:
+    """
+    A stream of lines helper class.
+    """
+
+    def __init__(self, string):
+        self.string = string.split("\n")
+        self.last_ptr = -1
+        self.cur_ptr = -1
+
+    def __repr__(self):
+        return self.string
+
+    def __str__(self):
+        return self.string
+
+    def peek(self):
+        if self.cur_ptr + 1 < len(self.string):
+            return self.string[self.cur_ptr + 1]
+        return None
+
+    def consume(self):
+        self.cur_ptr += 1
+
+    def commit(self):
+        self.last_ptr = self.cur_ptr
+
+    def rollback(self):
+        self.cur_ptr = self.last_ptr
+
 
 class CharacterStream:
     """
     A stream of characters helper class.
     """
+
     def __init__(self, string):
         self.string = string
         self.last_ptr = -1
@@ -25,8 +57,8 @@ class CharacterStream:
         return self.string
 
     def peek(self):
-        if self.cur_ptr+1 < len(self.string):
-            return self.string[self.cur_ptr+1]
+        if self.cur_ptr + 1 < len(self.string):
+            return self.string[self.cur_ptr + 1]
         return None
 
     def consume(self):
@@ -44,10 +76,11 @@ class Scanner:
     A simple Finite State Automaton simulator.
     Used for scanning an input stream.
     """
+
     def __init__(self, stream):
         self.set_stream(stream)
-        self.current_state=None
-        self.accepting_states=[]
+        self.current_state = None
+        self.accepting_states = []
 
     def set_stream(self, stream):
         self.stream = stream
@@ -67,7 +100,8 @@ class Scanner:
 
                 if __trace__:
                     if self.current_state is not None:
-                        print("transition", self.current_state, "-|", next_char, end=' ')
+                        print("transition", self.current_state, "-|", next_char,
+                              end=' ')
 
                 # perform transition and its action to the appropriate new state
                 next_state = self.transition(self.current_state, next_char)
@@ -77,7 +111,6 @@ class Scanner:
                         print("")
                     else:
                         print("|->", next_state)
-
 
                 # stop if a transition was not possible
                 if next_state is None:
@@ -101,138 +134,83 @@ class Scanner:
                 self.stream.rollback()
             return success
 
-## An example scanner, see http://msdl.cs.mcgill.ca/people/hv/teaching/SoftwareDesign/COMP304B2003/assignments/assignment3/solution/
-class NumberScanner(Scanner):
+
+class TrolleyIsOnJunctionScanner(Scanner):
     def __init__(self, stream):
         # superclass constructor
         super().__init__(stream)
 
-        self.value = 0
-        self.exp = 0
-        self.scale = 1
-
         # define accepting states
-        self.accepting_states=["S2","S4","S7"]
-
-    def __str__(self):
-        return str(self.value) + "E" + str(self.exp)
+        self.accepting_states = ["end"]
 
     def transition(self, state, input):
         """
         Encodes transitions and actions
+        :param state: The state in which we are currently
+        :param input: The input we get
+        :return: The state in which we are next given the input-parameters
         """
         if state is None:
-            # action
-            # initialize variables
-            self.value = 0
-            self.exp = 0
-            # new state
-            return "S1"
-
-        elif state == "S1":
-            if input  == '.':
-                # action
-                self.scale = 0.1
-                # new state
-                return "S3"
-            elif '0' <= input <= '9':
-                # action
-                self.value = ord(input.lower()) - ord('0')
-                # new state
-                return "S2"
+            return "init"
+        elif state == "init":
+            if input == "E 3":
+                return "locked"
             else:
-                return None
+                return "init"
+        elif state == "eof":
+            return "eof"
+        elif state == "locked":
+            if input == "X 3":
+                return "init"
+            elif re.match("(R [12])|(E [12])", input):
+                return "locked"
 
-        elif state == "S2":
-            if input  == '.':
-                # action
-                self.scale = 0.1
-                # new state
-                return "S4"
-            elif '0' <= input <= '9':
-                # action
-                self.value = self.value * 10 + ord(input.lower()) - ord('0')
-                # new state
-                return "S2"
-            elif input.lower()  == 'e':
-                # action
-                self.exp = 1
-                # new state
-                return "S5"
-            else:
-                return None
 
-        elif state == "S3":
-            if '0' <= input <= '9':
-                # action
-                self.value += self.scale * (ord(input.lower()) - ord('0'))
-                self.scale /= 10
-                # new state
-                return "S4"
-            else:
-                return None
+class TrolleyPriorityScanner(Scanner):
+    def __init__(self, stream):
+        # superclass constructor
+        super().__init__(stream)
 
-        elif state == "S4":
-            if '0' <= input <= '9':
-                # action
-                self.value += self.scale * (ord(input.lower()) - ord('0'))
-                self.scale /= 10
-                # new state
-                return "S4"
-            elif input.lower()  == 'e':
-                # action
-                self.exp = 1
-                # new state
-                return "S5"
-            else:
-                return None
+        # define accepting states
+        self.accepting_states = ["end"]
 
-        elif state == "S5":
-            if input == '+':
-                # new state
-                return "S6"
-            elif input  == '-':
-                # action
-                self.exp = -1
-                # new state
-                return "S6"
-            elif '0' <= input <= '9':
-                # action
-                self.exp *= ord(input.lower()) - ord('0')
-                # new state
-                return "S7"
-            else:
-                return None
+    def transition(self, state, input):
+        """
+        Encodes transitions and actions
+        :param state: The state in which we are currently
+        :param input: The input we get
+        :return: The state in which we are next given the input-parameters
+        """
 
-        elif state == "S6":
-            if '0' <= input <= '9':
-                # action
-                self.exp *= ord(input.lower()) - ord('0')
-                # new state
-                return "S7"
-            else:
-                return None
+        if not re.match("[RXGE] [123]", input):
+            raise Exception("72 piece chicken")
 
-        elif state == "S7":
-            if '0' <= input <= '9':
-                # action
-                self.exp = self.exp * 10 + ord(input.lower()) - ord('0')
-                # new state
-                return "S7"
-            else:
-                return None
+        if state is None:
+            return "init"
 
-        else:
-            return None
+        elif state == "init":
+            if input == "E 1": return "Wait1"
+            elif input == "E 2": return "Wait2"
+            elif input is None: return "end"
+            else: return "init"
 
-    def entry(self, state, input):
-        pass
+        elif state == "Wait1":
+            if input == "G 1": return "init"
+            elif input == "G 2": return None
+            else: "Wait1"
+
+        elif state == "Wait2":
+            if input == "G 2": return "init"
+            elif input == "G 1": return None
+            else: "Wait2"
+
 
 if __name__ == "__main__":
-    stream = CharacterStream("123e-4")
-    scanner = NumberScanner(stream)
+    f = open("demofile.txt", "r")
+    stream = LineStream(f.read())
+    scanner = TrolleyPriorityScanner(stream)
     success = scanner.scan()
     if success:
-        print("Stream has been accepted. Number: %se%s" % (str(scanner.value), str(scanner.exp)))
+        print("Stream has been accepted.")
     else:
-        print("Stream not accepted")
+        print("Stream not accepted.")
