d33da0e23f20ea708a3b7280ea7a082e66364e45
thomas <thomas@dooms.eu>
2021-10-28 12:27:02 +0200

    hill climbing

--- 2-modelica/script.py
+++ 2-modelica/script.py
@@ -1,35 +1,109 @@
 import csv
 import os
 from typing import Optional
+from dataclasses import dataclass
+import numpy as np
 
 PATH = "PRT_PID_system_res.csv"
 CMD = "./PRT_PID_system"
 
-kp_range = (300, 350)
-ki_range = (0.5, 1.5)
-kd_range = (-20, 70)
+# What are you doing STEP_SIZE?
+# Step size is defined in terms of span(x_range) * STEP_SIZE
+START_STEP_SIZE = 0.1
+EPS = 1.0000001
+
+KP_RANGE = (300, 350)
+KI_RANGE = (0.5, 1.5)
+KD_RANGE = (-20, 70)
+
+KP_START = KP_RANGE[0] + (KP_RANGE[1] - KP_RANGE[0]) / 2
+KI_START = KI_RANGE[0] + (KI_RANGE[1] - KI_RANGE[0]) / 2
+KD_START = KD_RANGE[0] + (KD_RANGE[1] - KD_RANGE[0]) / 2
 
 
-def default_print():
+@dataclass
+class Point:
+    kp: float
+    ki: float
+    kd: float
+
+
+def print_headers():
     os.system(CMD)
-    print(open(PATH, newline='').read())
+    file = open(PATH, newline='')
+    print(csv.DictReader(file).fieldnames)
 
 
 def least_squares_speed(rows) -> Optional[float]:
-    # check if failed
-    if max(row['customPlant.x_psgr'] for row in rows) > 0.35:
-        return None
+    accumulator = 0
+    for row in rows:
+        x_psgr = float(row['customPlant.x_psgr'])
+        v_trolley = float(row['customPlant.v_trolley'])
+        v_ideal = float(row['lookUp.v_ideal'])
 
-    return sum(row['customPlant.v_trolley'] ** 2 for row in rows)
+        if x_psgr > 0.35:
+            return None
+
+        accumulator += (v_trolley - v_ideal)**2
+    return accumulator
 
 
-def evaluate(kp, ki, kd, cost_fn) -> Optional[float]:
-    os.system(CMD + " -overide")
+def get_step(x_range, step_size):
+    return (x_range[1] - x_range[0]) * step_size
+
+
+# avoid the previous one?
+def calc_neighbours(sample: Point, step_size):
+    kp_o = get_step(KP_RANGE, step_size)
+    ki_o = get_step(KI_RANGE, step_size)
+    kd_o = get_step(KD_RANGE, step_size)
+
+    return [
+        Point(sample.kp + kp_o, sample.ki, sample.kd),
+        Point(sample.kp - kp_o, sample.ki, sample.kd),
+        Point(sample.kp, sample.ki + ki_o, sample.kd),
+        Point(sample.kp, sample.ki - ki_o, sample.kd),
+        Point(sample.kp, sample.ki, sample.kd + kd_o),
+        Point(sample.kp, sample.ki, sample.kd - kd_o)
+    ]
+
+
+def hill_climb(start: Point, cost_fn):
+    c_value = evaluate(start, cost_fn)
+    c_sample = start
+    c_size = START_STEP_SIZE
+    c_step = 0
+
+    while c_size > START_STEP_SIZE / 100:
+        c_step += 1
+
+        neighbours = calc_neighbours(c_sample, c_size)
+        values = [evaluate(n, cost_fn) for n in neighbours]
+        index = np.argmax(values)
+
+        # we add an EPS to avoid bouncing back and forward the whole time
+        if values[index] > c_value + EPS:
+            c_sample = neighbours[index]
+            c_value = values[index]
+        else:
+            c_size = c_size / 2
+
+        print("step", c_step, ":", c_sample, "->", c_value)
+
+
+def evaluate(sample: Point, cost_fn) -> Optional[float]:
+    # Very cool conversions
+    cmd = f"{CMD} -override pid.Ti={sample.kp / sample.ki},pid.Td={sample.kd / sample.kp},pid.k={sample.kp} >/dev/null 2>&1"
+    os.system()
+
     file = open(PATH, newline='')
     rows = csv.DictReader(file)
-    return cost_fn(rows)
+    value = cost_fn(rows)
+    print("sample", sample, ":", value)
+    return value
 
 
 # ------- main code ------- #
 os.chdir("/home/thomas/brol/PRT_PID_system")
-default_print()
+hill_climb(Point(KP_START, KI_START, KD_START), least_squares_speed)
+# print_headers()
