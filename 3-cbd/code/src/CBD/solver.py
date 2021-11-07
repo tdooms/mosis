0e6076839fb4a8a92b397186c3e2910598475a31
Basil Rommens <basil.rommens@student.uantwerpen.be>
2021-11-07 14:30:40 +0100

    fixed indentation of solver.py and reformatted line that returns false if the block in the algebraic loop is of that type

--- 3-cbd/code/src/CBD/solver.py
+++ 3-cbd/code/src/CBD/solver.py
@@ -3,67 +3,67 @@ from .Core import CBD
 from .util import PYTHON_VERSION
 
 if PYTHON_VERSION == 3:
-	# Python 2 complient
-	from functools import reduce
+    # Python 2 complient
+    from functools import reduce
 
 
 # Superclass for possible additional solvers
 class Solver:
-	"""
-	Superclass that can solve algebraic loops.
+    """
+    Superclass that can solve algebraic loops.
 
-	Args:
-		logger (Logger):    The logger to use.
-	"""
-	def __init__(self, logger):
-		self._logger = logger
+    Args:
+        logger (Logger):    The logger to use.
+    """
+    def __init__(self, logger):
+        self._logger = logger
 
-	def checkValidity(self, path, component):
-		"""
-		Checks the validity of an algebraic loop.
+    def checkValidity(self, path, component):
+        """
+        Checks the validity of an algebraic loop.
 
-		Args:
-			path (str):         The path of the top-level block.
-			component (list):   The blocks in the algebraic loop.
-		"""
-		raise NotImplementedError()
+        Args:
+            path (str):         The path of the top-level block.
+            component (list):   The blocks in the algebraic loop.
+        """
+        raise NotImplementedError()
 
-	def constructInput(self, component, curIt):
-		"""
-		Constructs input for the solver.
+    def constructInput(self, component, curIt):
+        """
+        Constructs input for the solver.
 
-		Args:
-			component (list):   The blocks in the algebraic loop.
-			curIt (int):        The current iteration of the simulation.
+        Args:
+            component (list):   The blocks in the algebraic loop.
+            curIt (int):        The current iteration of the simulation.
 
-		See Also:
-			:func:`solve`
-		"""
-		raise NotImplementedError()
+        See Also:
+            :func:`solve`
+        """
+        raise NotImplementedError()
 
-	def solve(self, solverInput):
-		"""
-		Solves the algebraic loop.
+    def solve(self, solverInput):
+        """
+        Solves the algebraic loop.
 
-		Args:
-			solverInput:    The constructed input.
+        Args:
+            solverInput:    The constructed input.
 
-		See Also:
-			:func:`constructInput`
-		"""
-		raise NotImplementedError()
+        See Also:
+            :func:`constructInput`
+        """
+        raise NotImplementedError()
 
 
 class LinearSolver(Solver):
-	"""
-	Solves linear algebraic loops.
-	"""
-	def checkValidity(self, path, component):
-		if not self.__isLinear(component):
-			self._logger.fatal("Cannot solve non-linear algebraic loop.\nSelf: {}\nComponents: {}".format(path, component))
+    """
+    Solves linear algebraic loops.
+    """
+    def checkValidity(self, path, component):
+        if not self.__isLinear(component):
+            self._logger.fatal("Cannot solve non-linear algebraic loop.\nSelf: {}\nComponents: {}".format(path, component))
 
-	def __isLinear(self, strongComponent):
-		"""Determines if an algebraic loop describes a linear equation or not.
+    def __isLinear(self, strongComponent):
+        """Determines if an algebraic loop describes a linear equation or not.
 
         Args:
             strongComponent (list): The detected loop, in a list (of BaseBlock instances)
@@ -71,186 +71,188 @@ class LinearSolver(Solver):
         Returns:
             :class:`True` if the loop is linear, else :code:`False`.
         """
-		# TODO: TO IMPLEMENT
-		for block in strongComponent:
-			if block.getBlockType() in ["AdderBlock", "NegatorBlock"]:
-				continue
+        # TODO: TO IMPLEMENT
+        for block in strongComponent:
+            if block.getBlockType() in ["AdderBlock", "NegatorBlock"]:
+                continue
 
-			unknown = len([x for x in block.getDependencies(0) if x in strongComponent])
-			if unknown > 1 and block.getBlockType() == "ProductBlock":
-				return False
+            unknown = len([x for x in block.getDependencies(0) if x in strongComponent])
+            if unknown > 1 and block.getBlockType() == "ProductBlock":
+                return False
 
-			if block.getBlockType() in ["InverterBlock", "LessThanBlock", "ModuloBlock", "RootBlock", "EqualsBlock",
-										"NotBlock", "OrBlock", "AndBlock", "SequenceBlock"]:
-				return False
+            if block.getBlockType() in ["InverterBlock", "LessThanBlock",
+                                        "ModuloBlock", "RootBlock",
+                                        "EqualsBlock", "NotBlock", "OrBlock",
+                                        "AndBlock", "SequenceBlock"]:
+                return False
 
-		return True
+        return True
 
-	def constructInput(self, strongComponent, curIteration):
-		"""
-		Constructs input for a solver of systems of linear equations
-		Input consists of two matrices:
+    def constructInput(self, strongComponent, curIteration):
+        """
+        Constructs input for a solver of systems of linear equations
+        Input consists of two matrices:
 
-			- M1: coefficient matrix, where each row represents an equation of the system
-			- M2: result matrix, where each element is the result for the corresponding equation in M1
-		"""
-		# Initialize matrices with zeros
-		size = len(strongComponent)
-		M1 = Matrix(size, size)
-		M2 = [0] * size
+            - M1: coefficient matrix, where each row represents an equation of the system
+            - M2: result matrix, where each element is the result for the corresponding equation in M1
+        """
+        # Initialize matrices with zeros
+        size = len(strongComponent)
+        M1 = Matrix(size, size)
+        M2 = [0] * size
 
-		# block -> index of block
-		indexdict = dict()
+        # block -> index of block
+        indexdict = dict()
 
-		for i, block in enumerate(strongComponent):
-			indexdict[block] = i
+        for i, block in enumerate(strongComponent):
+            indexdict[block] = i
 
-		# Get low-level dependency
-		resolveBlock = lambda possibleDep, output_port: possibleDep if not isinstance(possibleDep, CBD) else possibleDep.getBlockByName(output_port)
+        # Get low-level dependency
+        resolveBlock = lambda possibleDep, output_port: possibleDep if not isinstance(possibleDep, CBD) else possibleDep.getBlockByName(output_port)
 
-		# Get list of low-level dependencies from n inputs
-		def getBlockDependencies2(block):
-			return (resolveBlock(b, output_port) for (b, output_port) in [block.getBlockConnectedToInput(x) for x in block.getInputPortNames()])
+        # Get list of low-level dependencies from n inputs
+        def getBlockDependencies2(block):
+            return (resolveBlock(b, output_port) for (b, output_port) in [block.getBlockConnectedToInput(x) for x in block.getInputPortNames()])
 
-		for i, block in enumerate(strongComponent):
-			if block.getBlockType() == "AdderBlock":
-				for external in [x for x in getBlockDependencies2(block) if x not in strongComponent]:
-					M2[i] -= external.getSignal()[curIteration].value
-				M1[i, i] = -1
+        for i, block in enumerate(strongComponent):
+            if block.getBlockType() == "AdderBlock":
+                for external in [x for x in getBlockDependencies2(block) if x not in strongComponent]:
+                    M2[i] -= external.getSignal()[curIteration].value
+                M1[i, i] = -1
 
-				for compInStrong in [x for x in getBlockDependencies2(block) if x in strongComponent]:
-					M1[i, indexdict[compInStrong]] += 1
-			elif block.getBlockType() == "ProductBlock":
-				# M2 can stay 0
-				M1[i, i] = -1
-				fact = reduce(lambda x, y: x * y, [x.getSignal()[curIteration].value for x in getBlockDependencies2(block) if x not in strongComponent])
-				for compInStrong in [x for x in getBlockDependencies2(block) if x in strongComponent]:
-					M1[i, indexdict[compInStrong]] += fact
-			elif block.getBlockType() == "NegatorBlock":
-				# M2 can stay 0
-				M1[i, i] = -1
-				possibleDep, output_port = block.getBlockConnectedToInput("IN1")
-				M1[i, indexdict[resolveBlock(possibleDep, output_port)]] = - 1
-			elif block.getBlockType() == "InputPortBlock":
-				# M2 can stay 0
-				M1[i, i] = 1
-				possibleDep, output_port = block.parent.getBlockConnectedToInput(block.getBlockName())
-				M1[i, indexdict[resolveBlock(possibleDep, output_port)]] = - 1
-			elif block.getBlockType() == "OutputPortBlock" or block.getBlockType() == "WireBlock":
-				# M2 can stay 0
-				M1[i, i] = 1
-				dblock = block.getDependencies(0)[0]
-				if isinstance(dblock, CBD):
-					oport = block.getLinksIn()['IN1'].output_port
-					dblock = dblock.getBlockByName(oport).getLinksIn()['IN1'].block
-				M1[i, indexdict[dblock]] = -1
-			elif block.getBlockType() == "DelayBlock":
-				# If a delay is in a strong component, this is the first iteration
-				assert curIteration == 0
-				# And so the dependency is the IC
-				# M2 can stay 0 because we have an equation of the type -x = -ic <=> -x + ic = 0
-				M1[i, i] = -1
-				possibleDep, output_port = block.getBlockConnectedToInput("IC")
-				dependency = resolveBlock(possibleDep, output_port)
-				assert dependency in strongComponent
-				M1[i, indexdict[dependency]] = 1
-			else:
-				self._logger.fatal("Unknown element '{}', please implement".format(block.getBlockType()))
-		return M1, M2
+                for compInStrong in [x for x in getBlockDependencies2(block) if x in strongComponent]:
+                    M1[i, indexdict[compInStrong]] += 1
+            elif block.getBlockType() == "ProductBlock":
+                # M2 can stay 0
+                M1[i, i] = -1
+                fact = reduce(lambda x, y: x * y, [x.getSignal()[curIteration].value for x in getBlockDependencies2(block) if x not in strongComponent])
+                for compInStrong in [x for x in getBlockDependencies2(block) if x in strongComponent]:
+                    M1[i, indexdict[compInStrong]] += fact
+            elif block.getBlockType() == "NegatorBlock":
+                # M2 can stay 0
+                M1[i, i] = -1
+                possibleDep, output_port = block.getBlockConnectedToInput("IN1")
+                M1[i, indexdict[resolveBlock(possibleDep, output_port)]] = - 1
+            elif block.getBlockType() == "InputPortBlock":
+                # M2 can stay 0
+                M1[i, i] = 1
+                possibleDep, output_port = block.parent.getBlockConnectedToInput(block.getBlockName())
+                M1[i, indexdict[resolveBlock(possibleDep, output_port)]] = - 1
+            elif block.getBlockType() == "OutputPortBlock" or block.getBlockType() == "WireBlock":
+                # M2 can stay 0
+                M1[i, i] = 1
+                dblock = block.getDependencies(0)[0]
+                if isinstance(dblock, CBD):
+                    oport = block.getLinksIn()['IN1'].output_port
+                    dblock = dblock.getBlockByName(oport).getLinksIn()['IN1'].block
+                M1[i, indexdict[dblock]] = -1
+            elif block.getBlockType() == "DelayBlock":
+                # If a delay is in a strong component, this is the first iteration
+                assert curIteration == 0
+                # And so the dependency is the IC
+                # M2 can stay 0 because we have an equation of the type -x = -ic <=> -x + ic = 0
+                M1[i, i] = -1
+                possibleDep, output_port = block.getBlockConnectedToInput("IC")
+                dependency = resolveBlock(possibleDep, output_port)
+                assert dependency in strongComponent
+                M1[i, indexdict[dependency]] = 1
+            else:
+                self._logger.fatal("Unknown element '{}', please implement".format(block.getBlockType()))
+        return M1, M2
 
-	def solve(self, solverInput):
-		M1, M2 = solverInput
-		n = M1.rows
-		indxc = [0] * n
-		indxr = [0] * n
-		ipiv = [0] * n
-		icol = 0
-		irow = 0
-		for i in range(n):
-			big = 0.0
-			for j in range(n):
-				if ipiv[j] != 1:
-					for k in range(n):
-						if ipiv[k] == 0:
-							nb = math.fabs(M1[j, k])
-							if nb >= big:
-								big = nb
-								irow = j
-								icol = k
-						elif ipiv[k] > 1:
-							raise ValueError("GAUSSJ: Singular Matrix-1")
-			ipiv[icol] += 1
-			if irow != icol:
-				for l in range(n):
-					M1[irow, l], M1[icol, l] = M1[icol, l], M1[irow, l]
-				M2[irow], M2[icol] = M2[icol], M2[irow]
-			indxr[i] = irow
-			indxc[i] = icol
-			if M1[icol, icol] == 0.0:
-				raise ValueError("GAUSSJ: Singular Matrix-2")
-			pivinv = 1.0 / M1[icol, icol]
-			M1[icol, icol] = 1.0
-			for l in range(n):
-				M1[icol, l] *= pivinv
-			M2[icol] *= pivinv
-			for ll in range(n):
-				if ll != icol:
-					dum = M1[ll, icol]
-					M1[ll, icol] = 0.0
-					for l in range(n):
-						M1[ll, l] -= M1[icol, l] * dum
-					M2[ll] -= M2[icol] * dum
+    def solve(self, solverInput):
+        M1, M2 = solverInput
+        n = M1.rows
+        indxc = [0] * n
+        indxr = [0] * n
+        ipiv = [0] * n
+        icol = 0
+        irow = 0
+        for i in range(n):
+            big = 0.0
+            for j in range(n):
+                if ipiv[j] != 1:
+                    for k in range(n):
+                        if ipiv[k] == 0:
+                            nb = math.fabs(M1[j, k])
+                            if nb >= big:
+                                big = nb
+                                irow = j
+                                icol = k
+                        elif ipiv[k] > 1:
+                            raise ValueError("GAUSSJ: Singular Matrix-1")
+            ipiv[icol] += 1
+            if irow != icol:
+                for l in range(n):
+                    M1[irow, l], M1[icol, l] = M1[icol, l], M1[irow, l]
+                M2[irow], M2[icol] = M2[icol], M2[irow]
+            indxr[i] = irow
+            indxc[i] = icol
+            if M1[icol, icol] == 0.0:
+                raise ValueError("GAUSSJ: Singular Matrix-2")
+            pivinv = 1.0 / M1[icol, icol]
+            M1[icol, icol] = 1.0
+            for l in range(n):
+                M1[icol, l] *= pivinv
+            M2[icol] *= pivinv
+            for ll in range(n):
+                if ll != icol:
+                    dum = M1[ll, icol]
+                    M1[ll, icol] = 0.0
+                    for l in range(n):
+                        M1[ll, l] -= M1[icol, l] * dum
+                    M2[ll] -= M2[icol] * dum
 
-		for l in range(n - 1, 0, -1):
-			if indxr[l] != indxc[l]:
-				for k in range(n):
-					M1[k, indxr[l]], M1[k, indxc[l]] = M1[k, indxc[l]], M1[k, indxr[l]]
+        for l in range(n - 1, 0, -1):
+            if indxr[l] != indxc[l]:
+                for k in range(n):
+                    M1[k, indxr[l]], M1[k, indxc[l]] = M1[k, indxc[l]], M1[k, indxr[l]]
 
-		return solverInput[1]
+        return solverInput[1]
 
 
 class Matrix:
-	"""Custom, efficient matrix class. This class is used for efficiency purposes.
+    """Custom, efficient matrix class. This class is used for efficiency purposes.
 
-		- Using a while/for loop is slow.
-		- Using :class:`[[0] * n] * n` will have n references to the same list.
-		- Using :class:`[[0] * size for _ in range(size)]` can be 5 times slower
-		  than this class!
+        - Using a while/for loop is slow.
+        - Using :class:`[[0] * n] * n` will have n references to the same list.
+        - Using :class:`[[0] * size for _ in range(size)]` can be 5 times slower
+          than this class!
 
-	Numpy could be used to even further increase efficiency, but this increases the
-	required dependencies for external hardware systems (that may not provide these options).
+    Numpy could be used to even further increase efficiency, but this increases the
+    required dependencies for external hardware systems (that may not provide these options).
 
-	Note:
-		Internally, the matrix is segmented into chunks of 500.000.000 items.
-	"""
-	def __init__(self, rows, cols):
-		self.rows = rows
-		self.cols = cols
-		self.size = rows * cols
-		self.__max_list_size = 500 * 1000 * 1000
-		self.data = [[0] * ((rows * cols) % self.__max_list_size)]
-		for r in range(self.size // self.__max_list_size):
-			self.data.append([0] * self.__max_list_size)
+    Note:
+        Internally, the matrix is segmented into chunks of 500.000.000 items.
+    """
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.size = rows * cols
+        self.__max_list_size = 500 * 1000 * 1000
+        self.data = [[0] * ((rows * cols) % self.__max_list_size)]
+        for r in range(self.size // self.__max_list_size):
+            self.data.append([0] * self.__max_list_size)
 
-	def __getitem__(self, idx):
-		absolute = idx[0] * self.cols + idx[1]
-		outer = absolute // self.__max_list_size
-		inner = absolute % self.__max_list_size
-		return self.data[outer][inner]
+    def __getitem__(self, idx):
+        absolute = idx[0] * self.cols + idx[1]
+        outer = absolute // self.__max_list_size
+        inner = absolute % self.__max_list_size
+        return self.data[outer][inner]
 
-	def __setitem__(self, idx, value):
-		absolute = idx[0] * self.cols + idx[1]
-		outer = absolute // self.__max_list_size
-		inner = absolute % self.__max_list_size
-		self.data[outer][inner] = value
+    def __setitem__(self, idx, value):
+        absolute = idx[0] * self.cols + idx[1]
+        outer = absolute // self.__max_list_size
+        inner = absolute % self.__max_list_size
+        self.data[outer][inner] = value
 
-	def __str__(self):
-		res = ""
-		for row in range(self.rows):
-			if len(res) > 0:
-				res += "\n"
-			res += "["
-			for col in range(self.cols):
-				res += "\t%8.4f" % self[row, col]
-			res += "\t]"
-		return res
+    def __str__(self):
+        res = ""
+        for row in range(self.rows):
+            if len(res) > 0:
+                res += "\n"
+            res += "["
+            for col in range(self.cols):
+                res += "\t%8.4f" % self[row, col]
+            res += "\t]"
+        return res
